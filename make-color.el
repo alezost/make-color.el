022860c8b645d4fa5e17ce49bf9a3613ef75b730
Alex Kost <alezost@gmail.com>
2014-01-20 12:14:44 +0400

    Add package commentary

--- make-color.el
+++ make-color.el
@@ -1,10 +1,10 @@
-;;; make-color.el --- 
+;;; make-color.el --- Update fg/bg color of a text sample by pressing r/g/b/... keys
 
 ;; Copyright (C) 2014 Alex Kost
 
 ;; Author: Alex Kost <alezost@gmail.com>
 ;; Created: 9 Jan 2014
-;; Version: 
+;; Version: 0.1
 ;; URL: http://github.com/alezost/make-color.el
 ;; Keywords: color
 
@@ -23,6 +23,38 @@
 
 ;;; Commentary:
 
+;; This package allows to find a color by pressing keys for smooth
+;; changing foreground/background color of any text sample.
+
+;; To manually install the package, copy this file to a directory from
+;; `load-path' and add the following to your init-file:
+;;
+;;   (autoload 'macol-make-color "make-color" nil t)
+;;   (autoload 'macol-switch-to-buffer "make-color" nil t)
+
+;; Usage: select any region and call "M-x macol-make-color".  You will
+;; see a buffer in `macol-mode'.  Select some text in it and press "n"
+;; to set this text for colorizing.  Now you can modify a color with the
+;; following keys:
+;;
+;;   - r/R, g/G, b/B - decrease/increase red, green, blue components
+;;   - c/C, m/M, y/Y - decrease/increase cyan, magenta, yellow components
+;;   - h/H, s/S, l/L - decrease/increase hue, saturation, luminance
+;;   - RET - change current color (prompt for a value)
+
+;; If you are satisfied with current color, press "k" to put the color
+;; into `kill-ring'.  Any time you can set a new probing region with
+;; "n".  Also you can switch between modifying background/foreground
+;; colors with "t".  See mode description ("C-h m") for other key
+;; bindings.
+
+;; Macol buffer is not read-only, so you can yank and delete text and
+;; undo the changes as you always do.
+
+;; Note: The package and a custom group are called "make-color";
+;; all functions and variables have a prefix "macol-".
+
+;; For full description, see <http://github.com/alezost/make-color.el>.
 
 ;;; Code:
 
